09a5580e2054ae1c570f8074a8815fd20c0df282
Idan Berman <idan1915@gmail.com>
2020-01-01 17:08:13 +0000

    Remove nest merge

--- src/infrastructure/database/consts/InterfaceToRepositoryImplementation.ts
+++
@@ -1,6 +0,0 @@
-import InterfaceId from 'src/domain/types-interfaces-identifiers';
-import { TypeormVendorsRepository } from '../repositories';
-
-export const InterfaceToRepositoryImplementation = {
-  [InterfaceId.VendorsRepository]: TypeormVendorsRepository,
-};
--- src/infrastructure/database/utils/RepositoriesInterfacesIdProvider.ts
+++
@@ -1,7 +0,0 @@
-import { InterfaceToRepositoryImplementation } from '../consts/InterfaceToRepositoryImplementation';
-
-export class RepositoriesInterfacesIdProvider {
-  static get(): string[] {
-    return Object.keys(InterfaceToRepositoryImplementation) as string[];
-  }
-}
